de4d0232d45b68a58e3e808d6091a793b09ba51c
tomantjon <tom.antjon@gmail.com>
2017-12-24 16:44:02 +0100

    Add Starter application

---
+++ SportsStore/SportsStore/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace SportsStore.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+       public override bool Equals(object obj)
+        {
+            Product p = obj as Product;
+            if (p == null) return false;
+            return p.ProductId == ProductId;
+        }     
+    }
+
+
+}
